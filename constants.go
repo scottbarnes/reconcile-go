7a64bfc45e1851d28c5f2115b357e976ebd577e8
Scott Barnes <scottreidbarnes@gmail.com>
2022-12-31 22:11:42 -0800

    Parallel SQLite record import working.

--- constants.go
+++ constants.go
@@ -2,3 +2,7 @@ package main
 
 // var CHUNKSIZE = int64(1000 * 1000 * 1000)
 var CHUNKSIZE = int64(10 * 1000 * 1000)
+
+// Set some SQLite options, per https://avi.im/blag/2021/fast-sqlite-inserts/
+// sqlite3 options at https://github.com/mattn/go-sqlite3#connection-string
+const DBNAME string = "reconcile-go.db?_sync=0&_journal=WAL"
--- openlibraryParser.go
+++ openlibraryParser.go
@@ -23,6 +23,15 @@ type OpenLibraryEdition struct {
 	isbn13 string
 }
 
+func NewOpenLibraryEdition(olid, ocaid, isbn10, isbn13 string) *OpenLibraryEdition {
+	return &OpenLibraryEdition{
+		olid:   olid,
+		ocaid:  ocaid,
+		isbn10: isbn10,
+		isbn13: isbn13,
+	}
+}
+
 // paths is a list of JSON paths parsed by buger/jasonparser.
 var paths = [][]string{
 	{"key"},
@@ -160,11 +169,79 @@ func getFirstIsbnFromArray(isbns []byte) (string, error) {
 	return parsedIsbns[0], innerErr
 }
 
-func addEditionsToDB(edition *OpenLibraryEdition, stmt *sql.Stmt) error {
-	_, err := stmt.Exec(&edition.olid, &edition.ocaid, &edition.isbn13)
+// addEditionToDBBatch uses batching for faster DB inserts.
+// Thanks to https://github.com/h12w/sqlite-benchmark/blob/master/main.go
+func addEditionToDBBatch(editionCh <-chan *OpenLibraryEdition, doneCh chan<- struct{}, db *sql.DB, batchSize int) error {
+	batch := make([]interface{}, 0, batchSize*3)
+	var batchTotal int64
+
+	// Insert statement; preallocate bytes to save a few seconds and then
+	// concatenate a string the length of the items being inserted.
+	// 11*batchSize is a rough approximation, given 3 items + punctuation.
+	insertBeginning := make([]byte, 0, 11*batchSize)
+	bufStmtFull := bytes.NewBuffer(insertBeginning)
+	bufStmtFull.WriteString("INSERT INTO ol (edition_id, ocaid, isbn_13) VALUES ")
+	for i := 0; i < batchSize; i++ {
+		if i > 0 {
+			bufStmtFull.WriteString(",")
+		}
+		bufStmtFull.WriteString("(?, ?, ?)")
+	}
+
+	// Prepared statement for speed increase.
+	insertStmt, err := db.Prepare(bufStmtFull.String())
 	if err != nil {
 		return err
 	}
 
+	// Handle full batches first. Blocks until editionCh is closed.
+	for edition := range editionCh {
+		// Keep adding items until the batch is batchSize, then process.
+		if len(batch)/3 < batchSize {
+			batch = append(batch, edition.olid, edition.ocaid, edition.isbn13)
+			batchTotal++
+		}
+
+		// Insert when the batch is full
+		if len(batch)/3 == batchSize {
+			if _, err = insertStmt.Exec(batch...); err != nil {
+				return err
+			}
+
+			// "Reset" batch for next round.
+			batch = batch[0:0]
+		}
+	}
+
+	if err = insertStmt.Close(); err != nil {
+		return err
+	}
+
+	// With editionCh closed, it's time to handle the final, partially
+	// filled batch.
+	bufStmtFinal := bytes.NewBufferString("INSERT INTO ol (edition_id, ocaid, isbn_13) VALUES ")
+	for i := 0; i < len(batch)/3; i++ {
+		if i > 0 {
+			bufStmtFinal.WriteString(",")
+		}
+		bufStmtFinal.WriteString("(?, ?, ?)")
+	}
+
+	insertStmtFnl, err := db.Prepare(bufStmtFinal.String())
+	if err != nil {
+		return err
+	}
+
+	if _, err = insertStmtFnl.Exec(batch...); err != nil {
+		return err
+	}
+
+	err = insertStmtFnl.Close()
+	if err != nil {
+		return err
+	}
+
+	// Close done for both getEditions and runSeek in general.
+	defer close(doneCh)
 	return nil
 }
